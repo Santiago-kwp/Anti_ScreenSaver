def1e9a29e3aa0cb380ff578d5a8ced442f519cb
박기웅 <pgw10243@gmail.com>
2023-04-12 00:48:30 +0900

    left and right 추가

---
+++ left_right.py
@@ -0,0 +1,21 @@
+import time
+import pyautogui
+from datetime import datetime
+
+def saver() :
+    screenWidth, screenHeight = pyautogui.size() 
+	#화면 전체 크기를 출력(화면 해상도) 
+    print('{}, {}'.format(screenWidth, screenHeight)) 
+    cnt = 0 
+
+    while True :
+        # t 라는 문자를 입력
+        pyautogui.press('left',presses=1)
+        pyautogui.press('right',presses=1) 
+        cnt += 1 
+        print('{} 번째 동작중'.format(cnt)) 
+        time.sleep(120) 
+        print(datetime.now())
+
+if __name__ =="__main__" : 
+	saver()
